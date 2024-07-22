72efa375b91efbed28bacc939ffa641b603e34d8
Sveppg <54738234+Sveppg@users.noreply.github.com>
2024-07-22 15:20:34 +0200

    Update ParityCalc_tb.vhdl

--- ParityCalc_tb.vhdl
+++ ParityCalc_tb.vhdl
@@ -5,7 +5,7 @@ ENTITY ParityCalc_tb is
     END ParityCalc_tb;
 architecture testbench OF ParityCalc_tb IS
 
-COMPONENT ParityCalc_tb IS
+COMPONENT ParityCalc IS
     Port (
         x : in std_logic_vector(2 DOWNTO 0);
         y : OUT std_logic_vector(3 DOWNTO 0)
@@ -20,23 +20,23 @@ PORT MAP (x => x,
         x0STIM : PROCESS
         begin
             x(0) <= '0'; WAIT FOR 10 ns;
-            x(0) <= 'x'; WAIT FOR 3 ns;
+            x(0) <= 'X'; WAIT FOR 3 ns;
             x(0) <= '1'; WAIT FOR 10 ns;
-            x(0) <= 'x'; WAIT FOR 3 ns;
+            x(0) <= 'X'; WAIT FOR 3 ns;
         END PROCESS;
 
         x1STIM : PROCESS
         begin
             x(1) <= '0' ; WAIT FOR 17 ns;
-            x(1) <= 'x' ; WAIT FOR 3 ns;
+            x(1) <= 'X' ; WAIT FOR 3 ns;
             x(1) <= '1' ; WAIT FOR 17 ns;
-            x(1) <= 'x' ; WAIT FOR 3 ns;
+            x(1) <= 'X' ; WAIT FOR 3 ns;
         END PROCESS;
 
         x2STIM : PROCESS
         begin
             x(2) <= '0' ; WAIT FOR 25 ns;
-            x(2) <= 'x' ; WAIT FOR 3 ns;
+            x(2) <= 'X' ; WAIT FOR 3 ns;
             x(2) <= '1' ; WAIT FOR 25 ns;
             x(2) <= '1' ; WAIT FOR 3 ns;
         END PROCESS;
